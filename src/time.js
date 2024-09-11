6c1e2abb3e2d7bc1347ac20342e52b98e919264f
test <test@MacBook-Pro-Malik.local>
2024-09-11 20:35:58 +0500

    refactor module time js

--- src/time.js
+++ src/time.js
@@ -1,40 +1,25 @@
-import { computed, ref, watchEffect } from "vue"
-import {
- HUNDRED_PERCENT,
- MILLISECONDS_IN_SECONDS,
- MINUTES_IN_HOUR,
- SECONDS_IN_DAY,
- SECONDS_IN_MINUTES
-} from "@/constants.js"
-
-function calculateSecondsSinceMidnight() {
- const now = new Date()
-
- return (
-  SECONDS_IN_MINUTES * MINUTES_IN_HOUR * now.getHours() +
-  SECONDS_IN_MINUTES * now.getMinutes() +
-  now.getSeconds()
- )
-}
-
-const secondsSinceMidnight = ref(calculateSecondsSinceMidnight())
+import { computed, ref } from "vue"
+import { HUNDRED_PERCENT, MILLISECONDS_IN_SECONDS, SECONDS_IN_DAY } from "@/constants.js"
 
 export const secondsSinceMidnightInPercentage = computed(() => {
  return (HUNDRED_PERCENT * secondsSinceMidnight.value) / SECONDS_IN_DAY
 })
+
+export const now = ref(new Date())
+
+const midnight = computed(() => new Date(now.value).setHours(0, 0, 0, 0))
+
+const secondsSinceMidnight = computed(() => (now.value - midnight.value) / MILLISECONDS_IN_SECONDS)
 let timer = null
 
 export function startTimer() {
- secondsSinceMidnight.value = calculateSecondsSinceMidnight()
- timer = setInterval(() => (secondsSinceMidnight.value += 5 * 60), MILLISECONDS_IN_SECONDS)
+ secondsSinceMidnight.value = new Date()
+
+ timer = setInterval(() => {
+  now.value = new Date(now.value.getTime() + 5 * 60 * MILLISECONDS_IN_SECONDS)
+ }, MILLISECONDS_IN_SECONDS)
 }
 
 export function stopTimer() {
  clearInterval(timer)
 }
-
-watchEffect(() => {
- if (secondsSinceMidnight.value > SECONDS_IN_DAY) {
-  secondsSinceMidnight.value = 0
- }
-})
